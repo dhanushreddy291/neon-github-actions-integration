b4fa3afdc9e621b3fabe68d1c2be5a7a3b49d40e
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2024-11-30 16:00:12 +0000

    Add todo app

---
+++ app/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, text, boolean, pgTable, timestamp } from 'drizzle-orm/pg-core';
+
+export const todo = pgTable('todo', {
+    id: integer('id').primaryKey(),
+    text: text('text').notNull(),
+    done: boolean('done').default(false).notNull(),
+    created_at: timestamp('created_at').notNull().defaultNow(),
+});
