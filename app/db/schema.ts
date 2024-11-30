0781029ef52ec55bde2bf1fff1518151ce62acc5
Dhanush Reddy <29dhanushreddy@gmail.com>
2024-11-30 11:55:28 +0530

    feat: add created_at field to todo

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,7 +1,8 @@
-import { integer, text, boolean, pgTable } from 'drizzle-orm/pg-core';
+import { integer, text, boolean, pgTable, timestamp } from 'drizzle-orm/pg-core';
 
 export const todo = pgTable('todo', {
     id: integer('id').primaryKey(),
     text: text('text').notNull(),
-    done: boolean('done').default(false).notNull()
+    done: boolean('done').default(false).notNull(),
+    created_at: timestamp('created_at').notNull().defaultNow(),
 });
